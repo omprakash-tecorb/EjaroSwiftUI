b4451df7fb7bd7535e6d9a365672acdfed540620
Om Prakash <omprakash.2022@tecorb.co>
2022-08-29 16:11:30 +0530

    Initial Commit

---
+++ EjaroSwiftUI/EjaroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjaroSwiftUIApp.swift
+//  EjaroSwiftUI
+//
+//  Created by Apple on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct EjaroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
